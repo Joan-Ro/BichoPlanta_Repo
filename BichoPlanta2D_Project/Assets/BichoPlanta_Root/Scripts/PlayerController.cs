3015fd758897ee95694e1d1c0cf7e1f475b56261
Joan-Ro <jrogerd@outlook.es>
2026-01-19 10:48:06 +0100

    Animation Logic y Principios de Player Controller

---
+++ BichoPlanta2D_Project/Assets/BichoPlanta_Root/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    //Variables de referencia
+    private Rigidbody2D playerRB;
+    private Animator anim;
+    private float horizontalInput;
+
+
+    //Variables de estadisticas del player
+    public float speed;
+    public float jumpforce;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        Jump();
+    }
+
+    void Movement()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        playerRB.velocity = new Vector2();
+    }
+    void Jump ()
+    {
+
+
+    }
+}
